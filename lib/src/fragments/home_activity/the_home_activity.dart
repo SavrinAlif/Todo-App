32966fa90ac1c5bc1c589f5662a37fc9a0eaca65
Savrin Sharif <aerolite.alif@gmail.com>
2024-02-21 18:10:29 +0600

    TodoActivity added (Add and Remove) operations

--- lib/src/fragments/home_activity/the_home_activity.dart
+++ lib/src/fragments/home_activity/the_home_activity.dart
@@ -9,47 +9,121 @@ class TheHomeActivity extends StatefulWidget {
 }
 
 class _TheHomeActivityState extends State<TheHomeActivity> {
+
+  List toDoList = [];
+  String items = "";
+
+  theOnChangeActivity (content) {
+    setState(() {
+      items = content;
+    });
+  }
+
+  theTodoActivity () {
+    setState(() {
+      toDoList.add({'item':items});
+    });
+  }
+
+  theCardRemove (index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child:
-                Image.asset(
-                  'assets/images/to_do(512x512).png',
-                  scale: 8,
-                  ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child:
+              Image.asset(
+                'assets/images/to_do(512x512).png',
+                scale: 8,
               ),
-              const Text('Todo App',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          flexibleSpace: theBuildClipRRect(),
-        ),
-        body: Container(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: [
-                Expanded(flex: 10, child: Row(
-                  children: [
-                    Expanded(flex: 80,child: TextFormField(decoration: theTextFormFieldDecoration('Make a list'),)),
-                    const Expanded(flex:2,child: SizedBox(width: 10,)),
-                    Expanded(flex:20,child: ElevatedButton(onPressed: () {}, child: const Icon(Icons.add),)),
-                  ],
-                )),
-                const SizedBox(height: 40,),
-                const Expanded(flex: 90, child: Text('List')),
-              ],
             ),
+            const Text('Todo App',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
+        flexibleSpace: theBuildClipRRect(),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Expanded(flex: 10, child: Row(
+              children: [
+                Expanded(flex: 75,
+                    child: TextFormField(
+                      decoration: theTextFormFieldDecoration('Make a list'),
+                      onChanged: (content) {
+                        theOnChangeActivity(content);
+                        },
+                    )
+                ),
+                const Expanded(flex: 2, child: SizedBox(width: 10,)),
+                Expanded(
+                  flex: 25,
+                  child:
+                  ElevatedButton(
+                    style: theBuildStyleFrom(),
+                    onPressed: () {
+                      theTodoActivity();
+                    },
+                    child: const Icon(Icons.add, size: 35,),
+                  ),
+                ),
+              ],
+            )),
+            const SizedBox(height: 40,),
+             Expanded(
+                flex: 90,
+                child: ListView.separated(
+                    itemCount: toDoList.length,
+                    separatorBuilder: (context, index) => const Divider(),
+                    itemBuilder: (context, index) {
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)
+                        ),
+                        elevation: 10,
+                        child: theSizedBox(
+                            Row(
+                              children: [
+                                Expanded(
+                                    flex: 80,
+                                    child: Text(
+                                        toDoList[index]['item'].toString()
+                                    )
+                                ),
+                                Expanded(
+                                    flex: 20,
+                                    child: TextButton(
+                                      onPressed: (){
+                                        theCardRemove(index);
+                                        },
+                                      child: const Icon(Icons.remove_circle_outline),
+                                    )
+                                ),
+                              ],
+                            )
+                        ),
+                      );
+                    }
+                )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
