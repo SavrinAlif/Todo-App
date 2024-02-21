1ff319e8042e3f072f1d6f0c02e8723d81f600b2
Savrin Sharif <aerolite.alif@gmail.com>
2024-02-21 18:41:49 +0600

    SnackBar added

---
+++ lib/src/fragments/snack_bar_activity/the_snack_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void theSnackBar(message, context) {
+   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+         message,
+         style: const TextStyle(fontSize: 20),
+      ),
+      backgroundColor: Colors.red,
+      elevation: 10,
+      showCloseIcon: true,
+      closeIconColor: Colors.white,
+      duration: const Duration(milliseconds: 3000),
+      width: 500,
+      padding: const EdgeInsets.symmetric(
+         horizontal: 8.0, // Inner padding for SnackBar content.
+      ),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+         borderRadius: BorderRadius.circular(20.0),
+      ),
+   ));
+}
